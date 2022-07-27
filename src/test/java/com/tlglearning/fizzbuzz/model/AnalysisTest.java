e85c3fa04e6cd68543070121db593bdd833618b8
alice <aliceahuh213@email.com>
2022-07-27 15:44:25 -0400

    Implemented parameterized tests w/ spec change.

---
+++ src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
@@ -0,0 +1,67 @@
+package com.tlglearning.fizzbuzz.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.tlglearning.fizzbuzz.FizzBuzz;
+import java.util.EnumSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class AnalysisTest {
+  static final Set<State> fizzExpected = EnumSet.of(State.FIZZ);
+  static final Set<State> buzzExpected = EnumSet.of(State.BUZZ);
+
+
+  final Analysis analysis = new Analysis();
+//  private Analysis analysis;
+
+
+  //everytime instance will be created anyways, so don'e need to do below
+//  @BeforeEach
+//  void setUp() {
+//    analysis = new Analysis();
+//  }
+
+
+  @ParameterizedTest
+  @ValueSource(ints = {3, 21 ,999_999})
+  void analyze_fizz(int value) {
+    assertEquals(fizzExpected, analysis.analyze(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {5, 100, 100_000})
+  void analyze_buzz(int value) {
+    assertEquals(buzzExpected, analysis.analyze(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {0,15,999_999_990})
+  void analyze_fizzBuzz() {
+    Set<State> expected = EnumSet.of(State.FIZZ, State.BUZZ);
+    assertEquals(expected, analysis.analyze(15));
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "neither.csv", numLinesToSkip = 1)
+  void analyze_neither(int value) {
+    Set<State> expected = EnumSet.noneOf(State.class);
+    assertEquals(expected, analysis.analyze(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-1, -3, -5, -1})
+  void analyze_exception(int value) {
+    try {
+      analysis.analyze(value);
+      fail();
+    } catch (IllegalArgumentException e) {
+      // Do nothing; this is the expected behavior.
+    }
+  }
+
+}
