7216b1093d4e465a754655f4fa2a74b5c802e55b
alice <aliceahuh213@email.com>
2022-07-29 13:18:41 -0400

    InvalidInvocation is top-level class.

--- src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
+++ src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.EnumSet;
 import java.util.Set;
-import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -54,20 +53,6 @@ class AnalysisTest {
   @ParameterizedTest
   @ValueSource(ints = {-1, -3, -5, -1})
   void analyze_negative(int value) {
-    assertThrows(IllegalArgumentException.class, new InvalidInvocation(value));
-  }
-  //nested class
-  private class InvalidInvocation implements Executable {
-
-    private final int value;
-
-    public InvalidInvocation(int value) {
-      this.value = value;
-    }
-
-    @Override
-    public void execute() throws Throwable {
-      analysis.analyze(value);
-    }
+    assertThrows(IllegalArgumentException.class, new InvalidInvocation(analysis, value));
   }
 }
